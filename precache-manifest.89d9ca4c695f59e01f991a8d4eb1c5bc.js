818a1700809a770b4d8447b50e70332438d4e156
Kathy <kathrcardoza@gmail.com>
2019-04-26 21:18:00 -0700

    Updates

---
+++ precache-manifest.89d9ca4c695f59e01f991a8d4eb1c5bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da7aa8a2d1e9f3026636a33ed0845fc4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "27a85853a46bb93eaa0b",
+    "url": "/clicky-game/static/css/main.0d9bf0f0.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "27a85853a46bb93eaa0b",
+    "url": "/clicky-game/static/js/main.63761e8a.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
